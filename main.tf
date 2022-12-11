0eaad7343c8f2a26894d27e12a89138b62e3927f
rcbest <rcserveradm@gmail.com>
2022-12-12 01:08:03 +0200

    END_1

--- main.tf
+++ main.tf
@@ -56,12 +56,14 @@ resource "aws_security_group" "myapp-sg" {
     protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
+
   ingress {
     from_port   = 8080
     to_port     = 8080
     protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
+
   egress {
     from_port       = 0
     to_port         = 0
@@ -72,5 +74,5 @@ resource "aws_security_group" "myapp-sg" {
   }
   tags = {
     Name : "${var.env_prefix}-sg"
-
+  }
 }
