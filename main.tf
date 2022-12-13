f23365c25d87044d86ead52111722dedce2ee8f3
rcbest <rcserveradm@gmail.com>
2022-12-13 13:04:43 +0200

    mod5

--- main.tf
+++ main.tf
@@ -2,14 +2,6 @@ provider "aws" {
   region = "eu-central-1"
 }
 
-variable "vpc_cidr_block" {}
-variable "subnet_cidr_block" {}
-variable "avail_zone" {}
-variable "env_prefix" {}
-variable "instance_type" {}
-variable "public_key_location" {}
-#variable "privat_key_location" {}
-
 resource "aws_vpc" "myapp-vpc" {
   cidr_block = var.vpc_cidr_block
   tags = {
@@ -17,38 +9,6 @@ resource "aws_vpc" "myapp-vpc" {
   }
 }
 
-resource "aws_subnet" "myapp-subnet-1" {
-  vpc_id            = aws_vpc.myapp-vpc.id
-  cidr_block        = var.subnet_cidr_block
-  availability_zone = var.avail_zone
-  tags = {
-    Name : "${var.env_prefix}-subnet-1"
-  }
-}
-resource "aws_route_table" "myapp-route-table" {
-  vpc_id = aws_vpc.myapp-vpc.id
-  route {
-    cidr_block = "0.0.0.0/0"
-    gateway_id = aws_internet_gateway.myapp-igw.id
-  }
-  tags = {
-    Name : "${var.env_prefix}-rtb"
-
-  }
-}
-
-resource "aws_internet_gateway" "myapp-igw" {
-  vpc_id = aws_vpc.myapp-vpc.id
-  tags = {
-    Name : "${var.env_prefix}-igw"
-  }
-}
-
-resource "aws_route_table_association" "myapp-rta" {
-  subnet_id      = aws_subnet.myapp-subnet-1.id
-  route_table_id = aws_route_table.myapp-route-table.id
-
-}
 resource "aws_security_group" "myapp-sg" {
   name   = "myapp-sg"
   vpc_id = aws_vpc.myapp-vpc.id
@@ -93,13 +53,7 @@ data "aws_ami" "latest-amazon-linux-image" {
   }
 
 }
-output "ami-name" {
-  value = data.aws_ami.latest-amazon-linux-image.id
-}
-output "public-ip" {
-  value = aws_instance.myapp-server.public_ip
 
-}
 /*resource "aws_key_pair" "ssh-key" {
   key_name   = "AWS_key"
   public_key = file(var.public_key_location)
