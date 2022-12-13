bd26e585b32a68d4af617509078f71e401a4cdfd
rcbest <rcserveradm@gmail.com>
2022-12-13 13:40:29 +0200

    newm2

--- modules/subnet/main.tf
+++ modules/subnet/main.tf
@@ -1,5 +1,5 @@
 resource "aws_subnet" "myapp-subnet-1" {
-  vpc_id            = aws_vpc.myapp-vpc.id
+  vpc_id            = var.vpc_id
   cidr_block        = var.subnet_cidr_block
   availability_zone = var.avail_zone
   tags = {
@@ -7,7 +7,7 @@ resource "aws_subnet" "myapp-subnet-1" {
   }
 }
 resource "aws_route_table" "myapp-route-table" {
-  vpc_id = aws_vpc.myapp-vpc.id
+  vpc_id = var.vpc_id
   route {
     cidr_block = "0.0.0.0/0"
     gateway_id = aws_internet_gateway.myapp-igw.id
@@ -19,7 +19,7 @@ resource "aws_route_table" "myapp-route-table" {
 }
 
 resource "aws_internet_gateway" "myapp-igw" {
-  vpc_id = aws_vpc.myapp-vpc.id
+  vpc_id = var.vpc_id
   tags = {
     Name : "${var.env_prefix}-igw"
   }
